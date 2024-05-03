3d89bfd4c3238599d50baf58df2e921b7cb6a8fe
Sybodon <58808836+Sybodon6422@users.noreply.github.com>
2024-05-02 20:18:39 -0400

    Update CombatCalculator.cs

--- Assets/Scripts/CombatCalculator.cs
+++ Assets/Scripts/CombatCalculator.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public static class CombatCalculator
 {
-    public static bool DoesHit(Advantage advantage, StatBlock attacker,StatBlock deffender, int attackerWeaponBonus)
+    public static HitType DoesHit(Advantage advantage, StatBlock attacker, StatBlock deffender, int attackerWeaponBonus, bool hasProficiency, int abilityMod)
     {
         int hitRoll = RollForHit(advantage);
-
         int profBonus = 0;
-        if(attacker.simpleWeaponsProficiency){profBonus = attacker.proficiencyBonus;}
-        int total = hitRoll + profBonus + attackerWeaponBonus;
-        if(total > deffender.armorClass)
+        if (hasProficiency) { profBonus = attacker.proficiencyBonus; }
+        int total = hitRoll + abilityMod + profBonus + attackerWeaponBonus;
+        if (hitRoll == 20)
         {
-            GameConsole.Instance.PrintToConsole("Hit! Unit rolled " + hitRoll + " + " + (profBonus+attackerWeaponBonus) +" ("+total+")" );
-            return true;
+            GameConsole.Instance.PrintToConsole("Critical Hit!" + attacker.name + hitRoll + " + " + (profBonus + attackerWeaponBonus + abilityMod) + " (" + total + ")");
+            return HitType.CriticalHit;
+        }
+        if (total > deffender.GetArmorClass())
+        {
+            GameConsole.Instance.PrintToConsole("Hit!" + attacker.name + hitRoll + " + " + (profBonus + attackerWeaponBonus + abilityMod) + " (" + total + ")" + "DF AC: " + deffender.GetArmorClass());
+            return HitType.Hit;
         }
         else
         {
-            GameConsole.Instance.PrintToConsole("Miss! Unit rolled " + hitRoll + " + " + (profBonus+attackerWeaponBonus) +" ("+total+")" );
-            return false;
+            GameConsole.Instance.PrintToConsole("Miss!" + attacker.name + hitRoll + " + " + (profBonus + attackerWeaponBonus + abilityMod) + " (" + total + ")");
+            return HitType.Miss;
         }
     }
 
-    public static int DamageDealtMelee(StatBlock attacker, Weapon weapon, bool usingVersatile, int bonus)
+    public static int DamageDealtMelee(StatBlock attacker, Weapon weapon, bool usingVersatile, int bonus, bool criticalHit, int abilityMod)
     {
         var damageRoll = 0;
-        if(!usingVersatile)
+        int diceAmmount;
+        if (criticalHit)
         {
-            for (int i = 0; i < weapon.diceAmmount; i++)
+            diceAmmount = weapon.diceAmmount*2;
+        }
+        else
+        {
+            diceAmmount = weapon.diceAmmount;
+        }
+        if (!usingVersatile)
+        {
+            for (int i = 0; i < diceAmmount; i++)
             {
                 damageRoll += Dice.Roll(weapon.dice);
             }
@@ -41,8 +54,29 @@ public static class CombatCalculator
             }
         }
 
-        GameConsole.Instance.PrintToConsole("Unit dealt " + (damageRoll + bonus) + "damage!");
-        return (damageRoll + bonus);
+        GameConsole.Instance.PrintToConsole("Unit dealt " + (damageRoll + bonus + abilityMod) + "damage!");
+        return (damageRoll + bonus + abilityMod);
+    }
+
+    public static int DamageDealtMelee(StatBlock attacker, CROSSEnemyDataCreature.CreatureAttack creatureAttack, int bonus, bool criticalHit, int abilityMod)
+    {
+        var damageRoll = 0;
+        int diceAmmount;
+        if (criticalHit)
+        {
+            diceAmmount = creatureAttack.damageDiceAmmount * 2;
+        }
+        else
+        {
+            diceAmmount = creatureAttack.damageDiceAmmount;
+        }
+        for (int i = 0; i < diceAmmount; i++)
+        {
+            damageRoll += Dice.Roll(creatureAttack.damageDice);
+        }
+
+        GameConsole.Instance.PrintToConsole("Creature dealt " + (damageRoll + bonus + abilityMod) + "damage!", Color.red);
+        return (damageRoll + bonus + abilityMod);
     }
 
     private static int RollForHit(Advantage advantage)
@@ -50,39 +84,46 @@ public static class CombatCalculator
         switch (advantage)
         {
             case Advantage.advantage:
-            {
-                var hitCheck1 = Dice.RollD20();
-                var hitCheck2 = Dice.RollD20();
+                {
+                    var hitCheck1 = Dice.RollD20();
+                    var hitCheck2 = Dice.RollD20();
 
-                if(hitCheck2 > hitCheck1)
-                {
-                    return hitCheck2;
+                    if (hitCheck2 > hitCheck1)
+                    {
+                        return hitCheck2;
+                    }
+                    else
+                    {
+                        return hitCheck1;
+                    }
                 }
-                else
-                {
-                    return hitCheck1;
-                }
-            }
             case Advantage.disadvantage:
-            {
-                var hitCheck1 = Dice.RollD20();
-                var hitCheck2 = Dice.RollD20();
+                {
+                    var hitCheck1 = Dice.RollD20();
+                    var hitCheck2 = Dice.RollD20();
 
-                if(hitCheck2 > hitCheck1)
-                {
-                    return hitCheck1;
+                    if (hitCheck2 > hitCheck1)
+                    {
+                        return hitCheck1;
+                    }
+                    else
+                    {
+                        return hitCheck2;
+                    }
                 }
-                else
-                {
-                    return hitCheck2;
-                }
-            }
             case Advantage.noadvantage:
-            {
-                return Dice.RollD20();
-            }
+                {
+                    return Dice.RollD20();
+                }
         }
 
         return Dice.RollD20();
+    }
+
+    public enum HitType
+    {
+        Hit,
+        Miss,
+        CriticalHit
     }
 }
